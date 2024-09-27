d3ea869b989a4d5f4656ee33caae2932d12ab7e2
henrique-castilho <henrique.castilho.pires@gmail.com>
2024-09-27 05:25:37 -0300

    implementa componente de cadastro com armazenamento local

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,12 +1,36 @@
+
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Cliente } from '../model/cliente';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
-export class CadastroComponent {
 
+export class CadastroComponent {
+  public mensagem: string = "";
+  public obj: Cliente = new Cliente();
+
+  public gravar(){
+    localStorage.setItem("cadastro", JSON.stringify(this.obj))
+    this.mensagem = "Cadastro atualizado com sucesso!";
+  }
+
+  public carregar(){
+    let json = localStorage.getItem("cadastro");
+    if(json == null){
+      window.location.href="./login"
+    } else {
+      this.obj = JSON.parse(json);
+    }
+  }
+
+  constructor(){
+    this.carregar();
+  }
 }
