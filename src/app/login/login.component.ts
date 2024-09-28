1a2c70619f05246836b616bae696e33e126e1a3f
henrique-castilho <henrique.castilho.pires@gmail.com>
2024-09-27 22:11:21 -0300

    Adicionar componente Login com funcionalidade básica de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../model/cliente';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  public mensagem: string = "";
+  public obj: Cliente = new Cliente()
+
+  public fazerLogin() {
+    if(this.obj.email == "admin@gmail.com" && this.obj.senha == "123456") {
+      localStorage.setItem("cadastro", JSON.stringify(this.obj));
+      window.location.href="./cadastro";
+    } else {
+      this.mensagem = "Email ou senha ivalidos !!!"
+      localStorage.removeItem("cadastro");
+    }
+  }
+
+  public novoCadastro(){
+    localStorage.setItem("cadastro", JSON.stringify(this.obj));
+    window.location.href="./cadastro";
+  }
 }
