a3f142284e9a64f44eb537081d74875ea6f81585
henrique-castilho <henrique.castilho.pires@gmail.com>
2024-09-28 01:55:39 -0300

    Adiciona funcionalidade de reenvio de instruções para recuperação de senha

--- src/app/esqueci-senha/esqueci-senha.component.ts
+++ src/app/esqueci-senha/esqueci-senha.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../model/cliente';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-esqueci-senha',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './esqueci-senha.component.html',
   styleUrl: './esqueci-senha.component.css'
 })
-export class EsqueciSenhaComponent {
 
+export class EsqueciSenhaComponent {
+  public mensagem: string = "";
+  public obj: Cliente = new Cliente();
+
+  public reenviar(){
+    if(this.obj.email == ""){
+      this.mensagem = "Preencha o campo e-mail.";
+    } else {
+      this.mensagem = "As instruções foram enviada para o email: " + this.obj.email;
+    }
+  }
 }
