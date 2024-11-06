b8efce4421cef6cb52c7005505bb2daf15d70079
thediaax <gabrieldias23.dev@gmail.com>
2024-11-06 01:00:47 -0300

    criação das entities

---
+++ Projeto_Loja_Back/src/main/java/com/fatec/projeto_loja_back/Entity/Cesta.java
@@ -0,0 +1,55 @@
+package com.fatec.projeto_loja_back.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Cesta {
+    @Id
+    private int codigo;
+    private Cliente cliente;
+    private double total;
+    private List<Item> itens = new ArrayList<>();
+
+
+    public Cesta(int codigo, Cliente cliente, double total, List<Item> itens) {
+        this.codigo = codigo;
+        this.cliente = cliente;
+        this.total = total;
+        this.itens = itens;
+    }
+
+    public Cesta(){}
+
+
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public Cliente getCliente() {
+        return cliente;
+    }
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+    public double getTotal() {
+        return total;
+    }
+    public void setTotal(double total) {
+        this.total = total;
+    }
+    public List<Item> getItens() {
+        return itens;
+    }
+    public void setItens(List<Item> itens) {
+        this.itens = itens;
+    }
+
+
+}
+
---
+++ Projeto_Loja_Back/src/main/java/com/fatec/projeto_loja_back/Entity/Item.java
@@ -0,0 +1,48 @@
+package com.fatec.projeto_loja_back.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Item {
+    @Id
+    private int codigo;
+    private Produto produto;
+    private int quantidade;
+    private double valor;
+
+
+    public Item(int codigo, Produto produto, int quantidade, double valor) {
+        this.codigo = codigo;
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+
+    public Item() {}
+
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public Produto getProduto() {
+        return produto;
+    }
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+    public int getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    public double getValor() {
+        return valor;
+    }
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
+
