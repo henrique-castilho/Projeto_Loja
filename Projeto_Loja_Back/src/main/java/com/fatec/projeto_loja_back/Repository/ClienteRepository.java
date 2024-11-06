b2be345a1e3cb09fe2d8f790cb0c4b06e57a91fe
henrique-castilho <henrique.castilho.pires@gmail.com>
2024-11-06 18:27:26 -0300

    Adiciona métodos de login e recuperação de senha no ClienteRepository

---
+++ Projeto_Loja_Back/src/main/java/com/fatec/projeto_loja_back/Repository/ClienteRepository.java
@@ -0,0 +1,20 @@
+package com.fatec.projeto_loja_back.Repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.fatec.projeto_loja_back.Entity.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+        
+    @Query(value = "SELECT * FROM cliente WHERE email=?1 AND senha=?2", nativeQuery = true)
+    Optional<Cliente> login(String email, String senha);
+
+    @Query(value = "SELECT * FROM cliente WHERE email=?1", nativeQuery = true)
+    Optional<Cliente> recuperaSenha(String email);
+    
+}
