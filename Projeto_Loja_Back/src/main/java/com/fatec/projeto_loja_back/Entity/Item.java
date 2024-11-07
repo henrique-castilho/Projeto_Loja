394902a43e313230bef3b6e20644c51617cbe73b
thediaax <gabrieldias23.dev@gmail.com>
2024-11-07 19:42:18 -0300

    Cria entity item e cesta

--- Projeto_Loja_Back/src/main/java/com/fatec/projeto_loja_back/Entity/Item.java
+++ Projeto_Loja_Back/src/main/java/com/fatec/projeto_loja_back/Entity/Item.java
@@ -1,15 +1,21 @@
 package com.fatec.projeto_loja_back.Entity;
 
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.Id;
+ import jakarta.persistence.*;
 
-// @Entity
+@Entity
 public class Item {
-    // @Id
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int codigo;
+    @ManyToOne
+    @JoinColumn(name = "produto_codigo", nullable = false)
     private Produto produto = new Produto();
+    @ManyToOne
+    @JoinColumn(name = "cesta_codigo", nullable = false)
+    private Cesta cesta = new Cesta();
     private int quantidade;
     private double valor;
+
     
 
     public Item(int codigo, Produto produto, int quantidade, double valor) {
@@ -24,26 +30,34 @@ public class Item {
     public int getCodigo() {
         return codigo;
     }
+
     public void setCodigo(int codigo) {
         this.codigo = codigo;
     }
+
     public Produto getProduto() {
         return produto;
     }
+
     public void setProduto(Produto produto) {
         this.produto = produto;
     }
+
     public int getQuantidade() {
         return quantidade;
     }
+
     public void setQuantidade(int quantidade) {
         this.quantidade = quantidade;
     }
+
     public double getValor() {
         return valor;
     }
+
     public void setValor(double valor) {
         this.valor = valor;
     }
+
 }
 
