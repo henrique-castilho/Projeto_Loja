f0c310a30f06acdf02525df94c59a5f0120daf05
henrique-castilho <henrique.castilho.pires@gmail.com>
2024-11-15 01:23:34 -0300

    Corrige os returns para integração com o front

--- Projeto_Loja_Back/src/main/java/com/fatec/projeto_loja_back/Controller/ProdutoController.java
+++ Projeto_Loja_Back/src/main/java/com/fatec/projeto_loja_back/Controller/ProdutoController.java
@@ -1,9 +1,11 @@
 package com.fatec.projeto_loja_back.Controller;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,21 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fatec.projeto_loja_back.Entity.Produto;
 import com.fatec.projeto_loja_back.Repository.ProdutoRepository;
 
+@CrossOrigin()
 @RestController
 public class ProdutoController {
     @Autowired
     ProdutoRepository bd;
 
     @PostMapping("/api/produto")
-    public String gravar(@RequestBody Produto obj) {
+    public Map<String, String> gravar(@RequestBody Produto obj) {
         bd.save(obj);
-        return "O produto " + obj.getNome() + "foi salvo corretamente";
+        return Map.of("mensagem", "O produto " + obj.getNome() + "foi salvo corretamente");
     }
 
     @PutMapping("/api/produto")
-    public String alterar(@RequestBody Produto obj) {
+    public Map<String, String> alterar(@RequestBody Produto obj) {
         bd.save(obj);
-        return "O produto " + obj.getNome() + " foi alterado corretamente";
+        return Map.of("mensagem","O produto " + obj.getNome() + " foi alterado corretamente");
     }
 
     @GetMapping("/api/produto/{codigo}")
@@ -43,12 +46,12 @@ public class ProdutoController {
     }
 
      @DeleteMapping("/api/produto/{codigo}")
-    public String remover(@PathVariable int codigo) {
+    public Map<String, String> remover(@PathVariable int codigo) {
         if (bd.existsById(codigo)) {
             bd.deleteById(codigo);
-            return "Registro " + codigo + " removido com sucesso!";
+            return null;
         } else {
-            return "Produto não encontrado";
+            return Map.of("mensagem","Produto não encontrado");
         }
     }
 
