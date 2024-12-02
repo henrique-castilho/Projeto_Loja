e88027cb48c4d72e6fa0309d7c078de1da505501
henrique-castilho <henrique.castilho.pires@gmail.com>
2024-12-02 03:33:48 -0300

    exibição e manipulação da cesta de compras, incluindo funções para adicionar/remover itens, calcular o total e realizar pedidos com confirmação.

--- Projeto_Loja_Front/src/app/cesta/cesta.component.ts
+++ Projeto_Loja_Front/src/app/cesta/cesta.component.ts
@@ -15,57 +15,63 @@ import { CestaService } from '../service/cesta.service'
 export class CestaComponent {
   public mensagem: string = "Sua cesta";
   public cesta: Cesta = new Cesta();
-  public mostrarConfirmacao: boolean = false; 
+  public mostrarConfirmacao: boolean = false;
   public total: number = 0;
 
-  constructor(private service: CestaService){
-    let json = localStorage.getItem("cesta");
-    if(json!=null) {
+  constructor(private service: CestaService) {
+    this.carregarCesta();
+  }
+
+  private carregarCesta() {
+    const json = localStorage.getItem("cesta");
+    if (json) {
       this.cesta = JSON.parse(json);
-    } else {
-      this.mensagem = "Cesta vazia, adicione novos itens!";
-    }
-  }
-
-  public removerItem(obj: Item) {
-    // Verifica se a quantidade do item é maior que 1
-    if (obj.quantidade > 1) {
-      // Diminui a quantidade do item
-      obj.quantidade -= 1;
-      // Atualiza o valor total da cesta
-      this.cesta.total -= obj.produto.valor;
-    } else {
-      // Se a quantidade for 1, remove o item completamente
-      this.cesta.itens = this.cesta.itens.filter(item => item !== obj);
-      // Atualiza o total da cesta
       this.atualizarTotal();
-    }
-  
-    // Salva a cesta no localStorage e atualiza a exibição
-    localStorage.setItem("cesta", JSON.stringify(this.cesta));
-    this.verificarCestaVazia();
-  }
-  
-  // Atualiza o total da cesta
-  private atualizarTotal() {
-    this.cesta.total = this.cesta.itens.reduce(
-      (acc, item) => acc + item.quantidade * item.produto.valor,
-      0
-    );
-  }
-  
-  // Verifica se a cesta está vazia e atualiza a mensagem
-  private verificarCestaVazia() {
-    if (this.cesta.itens.length === 0) {
-      this.mensagem = "Cesta vazia, adicione novos itens!";
+    } else {
+      this.mensagem = "Carrinho vazio, adicione novos itens!";
     }
   }
-  
 
-  public limparCesta(){
+  public removerItem(item: Item) {
+    const index = this.cesta.itens.findIndex(i => i.codigo === item.codigo);
+    if (index !== -1) {
+      const itemEncontrado = this.cesta.itens[index];
+      if (itemEncontrado.quantidade > 1) {
+        itemEncontrado.quantidade--;
+      } else {
+        this.cesta.itens.splice(index, 1);
+      }
+
+      this.atualizarTotal();
+      this.salvarCesta();
+    }
+
+    if (this.cesta.itens.length === 0) {
+      this.mensagem = "Carrinho vazio, adicione novos itens!";
+      this.limpar();
+    }
+  }
+
+  private atualizarTotal() {
+    this.total = this.cesta.itens.reduce((total, item) => total + (item.quantidade * item.produto.valor), 0);
+    this.cesta.total = this.total;
+  }
+
+  private limparCompra(novoPedido: Cesta) {
     localStorage.removeItem("cesta");
     this.cesta = new Cesta();
-    this.mensagem = "Cesta vazia, adicione novos itens!";
+    this.mensagem = `Compra realizada com sucesso! Pedido nº ${novoPedido.codigo}`;
+  }
+
+  public limpar() {
+    localStorage.removeItem("cesta");
+    this.cesta = new Cesta();
+    this.total = 0;
+    this.mensagem = "Carrinho vazio, adicione novos itens!";
+  }
+
+  private salvarCesta() {
+    localStorage.setItem("cesta", JSON.stringify(this.cesta));
   }
 
   public abrirConfirmacao() {
@@ -76,43 +82,46 @@ export class CestaComponent {
     this.mostrarConfirmacao = false;
   }
 
-  // public finalizarCompra() {
-  //   this.mensagem = "Compra finalizada com sucesso!";
-  //   localStorage.removeItem("cesta");
-  //   this.cesta = new Cesta();
-  //   this.mostrarConfirmacao = false;
-  // }
-
-  gravarPedido(){
-    let jsonCliente = localStorage.getItem("cliente");
-    if(jsonCliente != null) {
+  public gravarPedido() {
+    if (this.cesta.itens.length === 0) {
+      this.mensagem = "Carrinho vazio! Adicione itens antes de realizar o pedido.";
+      return;
+    }
+    const jsonCliente = localStorage.getItem("cliente");
+    if (jsonCliente) {
       this.cesta.cliente = JSON.parse(jsonCliente);
-      this.cesta.total = this.total;
       this.service.gravarPedido(this.cesta).subscribe({
-        next:(data)=>{
-          let novoPedido = data;
-          this.gravarItem(novoPedido);
+        next: (novoPedido) => {
+          console.log("Novo pedido", novoPedido)
+          this.gravarItens(novoPedido);
+          this.fecharConfirmacao();
         },
-        error:(err)=>{this.mensagem = "Ocorreu um erro tente mais tarde!";}
+        error: () => {
+          this.mensagem = "Ocorreu um erro ao gravar o pedido, tente novamente.";
+          this.fecharConfirmacao();
+        }
       });
     } else {
-      this.mensagem = "Faça o login para gravar o pedido !!!";
+      this.mensagem = "Faça o login para gravar o pedido!";
+      this.fecharConfirmacao();
     }
- }
+  }
 
- gravarItem(novoPedido: Cesta){
-    for(let obj of this.cesta.itens){
-        obj.codigoCesta = novoPedido.codigo
-    }
-    this.service.gravarItem(this.cesta.itens).subscribe({
-      next:(data)=>{
-        this.mensagem="Pedido "+ novoPedido.codigo + " gravado com sucesso!!!";
-        localStorage.removeItem("cesta");
-        this.cesta = new Cesta();
-        this.mostrarConfirmacao = false;
-      },
-      error:(err)=>{this.mensagem = "Ocorreu um erro tente mais tarde!";}
-  });
- }
+  gravarItens(novoPedido: Cesta) {
+    const novosItens = this.cesta.itens.map((item) => {
+        const novoItem = { ...item };
+        novoItem.codigo = 0;
+        novoItem.codigoCesta = novoPedido.codigo;
+        return novoItem;
+    });
 
-}
+    this.service.gravarItem(novosItens).subscribe({
+        next: (data) => {
+            this.limparCompra(novoPedido);
+        },
+        error: () => {
+            this.mensagem = "Erro ao gravar itens do pedido, tente novamente.";
+        }
+    });
+  }
+} 
