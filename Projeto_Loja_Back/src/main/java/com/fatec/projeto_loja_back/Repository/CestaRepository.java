7f38e1614e0ae4331f1be69412659ef567edb974
henrique-castilho <henrique.castilho.pires@gmail.com>
2024-12-02 03:32:12 -0300

    Adicionada a consulta codMaximo no CestaRepository para obter o maior código de cesta existente.

--- Projeto_Loja_Back/src/main/java/com/fatec/projeto_loja_back/Repository/CestaRepository.java
+++ Projeto_Loja_Back/src/main/java/com/fatec/projeto_loja_back/Repository/CestaRepository.java
@@ -2,8 +2,11 @@ package com.fatec.projeto_loja_back.Repository;
 
 import com.fatec.projeto_loja_back.Entity.Cesta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CestaRepository extends JpaRepository<Cesta, Integer> {
+    @Query("SELECT COALESCE(MAX(c.codigo), 0) FROM Cesta c")
+    int codMaximo();
 }
